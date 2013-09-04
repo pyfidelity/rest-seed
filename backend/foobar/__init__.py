71b0c7234225e23cf3fbf0fd29ad229fa97176f4
Andreas Zeidler <az@zitc.de>
2013-09-04 16:15:23 +0200

    allow running a development instance

--- backend/foobar/__init__.py
+++ backend/foobar/__init__.py
@@ -1,2 +1,18 @@
 # module for project-specific development
 # please rename and start hacking...
+
+from pyramid.settings import asbool
+from transaction import commit
+from restbase import configure, db_setup, utils
+
+
+project_name = utils.get_distribution().project_name
+
+
+def main(global_config, **settings):
+    config = configure(global_config, **settings)
+    if asbool(settings.get('debug', False)):
+        config.add_static_view('/', '%s:../../frontend/app/' % project_name)
+    db_setup(**settings)
+    commit()
+    return config.make_wsgi_app()
