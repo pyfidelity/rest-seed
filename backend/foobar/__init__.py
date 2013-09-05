5189858c3e66a3f2f6b4306477292eedb3e50b83
Andreas Zeidler <andi@pyfidelity.com>
2013-09-05 19:27:29 +0200

    demonstrate how 796ab042 was intended to allow additional configuration

--- backend/foobar/__init__.py
+++ backend/foobar/__init__.py
@@ -3,16 +3,24 @@
 
 from pyramid.settings import asbool
 from transaction import commit
-from restbase import configure, db_setup, utils
+from restbase import configure as base_configure, db_setup, utils
 
 
 project_name = utils.get_distribution().project_name
 
 
-def main(global_config, **settings):
-    config = configure(global_config, **settings)
+def configure(global_config, **settings):
+    config = base_configure(global_config, **settings)
+    # add additional configuration here...
     if asbool(settings.get('debug', False)):
         config.add_static_view('/', '%s:../../frontend/app/' % project_name)
+    config.scan()
+    config.commit()
+    return config
+
+
+def main(global_config, **settings):
+    config = configure(global_config, **settings)
     db_setup(**settings)
     commit()
     return config.make_wsgi_app()
