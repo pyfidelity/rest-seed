072e83af44ee7cf15b8e65b0c2724f4286b8e735
Andreas Zeidler <andi@pyfidelity.com>
2014-04-20 16:30:37 +0200

    add "password reset" service

---
+++ backend/restbase/tests/test_reset_password.py
@@ -0,0 +1,60 @@
+from pytest import fixture
+from pyquery import PyQuery
+from urllib import unquote
+from ..testing import route_url
+
+
+@fixture
+def reset_request_url(alice):
+    return route_url('password-reset', token='')
+
+
+def test_post_password_reset(browser, alice, reset_request_url, mailer):
+    data = dict(email=u'alice@foo.com')
+    result = browser.post_json(reset_request_url, data).json
+    assert result['status'] == 'success'
+    # but an email has been sent with the signup link...
+    message, = mailer.outbox
+    assert message.recipients == ['alice@foo.com']
+    assert 'Reset password' in message.html
+    assert 'Reset password' in message.body
+    # the mail contains the invitation link
+    a_tag = PyQuery(message.html)('a')[0]
+    link = unquote(a_tag.attrib['href'])
+    assert link.startswith('http://example.com/-/reset/')
+
+
+def test_post_password_reset_validation(browser, reset_request_url):
+    data = dict(email='ernie')
+    result = browser.post_json(reset_request_url, data, status=400).json
+    assert [(e['name'], e['description']) for e in result['errors']] == [
+        ('email', 'Email does not exist')]
+
+
+@fixture
+def reset_url(alice):
+    from ..views.reset_password import make_token
+    return route_url('password-reset', token=make_token(alice))
+
+
+def test_put_password_reset(browser, reset_url, alice):
+    data = dict(password='foobar')
+    result = browser.put_json(reset_url, data)
+    assert result.json['status'] == 'success'
+    assert alice.validate_password('foobar')
+
+
+def test_put_password_reset_to_bad_url(browser, reset_url, alice):
+    url = reset_url[:-3] + 'xxx'        # guess the signature!
+    data = dict(password='foobar')
+    browser.put_json(url, data, status=404)
+    # the password has not changed...
+    assert alice.validate_password('alice')
+
+
+def test_password_reset_link_works_only_once():
+    NotImplemented
+
+
+def test_expired_password_reset_link():
+    NotImplemented
---
+++ backend/restbase/views/reset_password.py
@@ -0,0 +1,49 @@
+from colander import MappingSchema, SchemaNode, String, Invalid
+from cornice.service import Service
+from pyramid_mailer import get_mailer
+
+from .. import _, principals, security, utils, path
+
+
+service = Service(name='password-reset', path=path('reset/{token:.*}'))
+
+
+def existing_user(node, param):
+    user = principals.find_user(param)
+    if user is not None and user.active:
+        return True
+    else:
+        raise Invalid(node, _('Email does not exist'))
+
+
+class Schema(MappingSchema):
+    email = SchemaNode(String(), title=u'Email', validator=existing_user)
+
+
+@service.post(schema=Schema, accept='application/json')
+def forgot_password(request):
+    user = principals.find_user(request.validated['email'])
+    send_password_reset_mail(user, request)
+    return dict(status='success')
+
+
+def make_token(user):
+    tokenizer = security.make_tokenizer(salt=service.name)
+    return tokenizer(dict(id=str(user.id), email=user.email))
+
+
+def send_password_reset_mail(user, request):
+    url = request.route_url(service.name, token=make_token(user))
+    message = utils.render_mail(request=request, template='password_reset',
+        recipients=[user.email], subject=_('Password reset'),
+        data=dict(user=user, url=url))
+    get_mailer(request).send(message)
+
+
+@service.put(accept='application/json')
+def reset_password(request):
+    factory = security.make_factory(salt=service.name)
+    query = factory(request)    # the token payload is used to find the user
+    user = principals.Principal.query.filter_by(**query).one()
+    user.update(password=request.json_body['password'])
+    return dict(status='success')
