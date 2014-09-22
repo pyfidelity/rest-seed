190d4aac1b7034e5073bde47df72fcd913c430b7
Andreas Zeidler <andi@pyfidelity.com>
2014-09-22 14:26:22 +0200

    make future merges easier...

--- backend/backrest/models/__init__.py
+++ backend/backrest/models/__init__.py
@@ -4,4 +4,9 @@ from .file import File
 from .content import Content, get_content
 
 
-__all__ = [Root, Base, metadata, db_session, File, Content, get_content]
+__all__ = [
+    Root,
+    Base, metadata, db_session,
+    File,
+    Content, get_content
+]
