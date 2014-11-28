79af1529a464ac38b160769931de12c95fb13dcb
Tom Lazar <tomster@pyfidelity.com>
2014-11-28 15:41:39 +0100

    include version in app info

--- backend/backrest/views/appinfo.py
+++ backend/backrest/views/appinfo.py
@@ -1,7 +1,8 @@
 from cornice.service import Service
+from pkg_resources import get_distribution
 from pyramid.settings import asbool
 
-from .. import path
+from .. import path, project_name
 
 
 app_info = Service(name='appinfo', path=path(''),
@@ -11,7 +12,7 @@ app_info = Service(name='appinfo', path=path(''),
 @app_info.get()
 def get_app_info(request):
     settings = request.registry.settings
-    result = dict(debug=asbool(settings.debug))
+    result = dict(debug=asbool(settings.debug), version=get_distribution(project_name).version)
     if request.user is None:
         result['authenticated'] = False
     else:
