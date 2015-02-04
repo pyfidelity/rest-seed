cf26cfd014e65d16cdc0c21c3f56b62acd3c92c4
Andreas Zeidler <andi@pyfidelity.com>
2015-02-04 18:31:25 +0100

    migrate existing paths to uuids and vice versa

--- backend/backrest/migrations/versions/1e0ff2269c43_add_file_uuids.py
+++ backend/backrest/migrations/versions/1e0ff2269c43_add_file_uuids.py
@@ -16,14 +16,18 @@ from sqlalchemy.dialects import postgresql
 
 
 def upgrade():
-    op.add_column('file', sa.Column('uuid', postgresql.UUID(as_uuid=True), nullable=False))
+    op.add_column('file', sa.Column('uuid', postgresql.UUID(as_uuid=True)))
+    op.execute('''UPDATE file SET uuid = uuid(split_part(path, '.', 1));''')
+    op.alter_column('file', 'uuid', nullable=False)
     op.create_index(op.f('ix_file_uuid'), 'file', ['uuid'], unique=True)
     op.drop_constraint(u'file_path_key', 'file', type_='unique')
     op.drop_column('file', 'path')
 
 
 def downgrade():
-    op.add_column('file', sa.Column('path', sa.VARCHAR(length=128), autoincrement=False, nullable=False))
+    op.add_column('file', sa.Column('path', sa.VARCHAR(length=128), autoincrement=False))
+    op.execute('''UPDATE file SET path = concat(uuid, substring(filename from '\.[^.]*$'));''')
+    op.alter_column('file', 'path', nullable=False)
     op.create_unique_constraint(u'file_path_key', 'file', ['path'])
     op.drop_index(op.f('ix_file_uuid'), table_name='file')
     op.drop_column('file', 'uuid')
